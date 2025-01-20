cbf0edff0c6d9714348c58ff0c017e6ec26d1f6f
Mikhail Klyshnikov <mishakurg@gmail.com>
2025-01-20 11:36:12 +0300

    uppgrade dmp layer

--- dmp_service/dmp.py
+++ dmp_service/dmp.py
@@ -192,7 +192,7 @@ def dmp_seats():
                 s6 = row["fare_conditions"]
                 
                 l2 = h1
-                l3 = 1
+                l3 = row["aircraft_code"]
                 l4 = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                 l5 = "kafka"
                 l1 = str(hash(l2) + hash(l3) + hash(l4) + hash(l5))
@@ -210,12 +210,22 @@ def dmp_seats():
             	"""
                 cursor.execute(sql_sat, (s1, s2, s3, s4, s5, s6))
                 
-                sql_lnk = """
-                    INSERT INTO lnk_seat_on_aircraft (lnk_seat_on_aircraft, hub_seat_key, hub_aircraft_key, lnk_load_dts, lnk_rec_src)
-                    VALUES (%s, %s, SELECT h.hub_aircraft_key FROM hub_aircraft as h WHERE h.hub_aircraft_bk = %s, %s, %s)
+                pg_connector.commit()
+                
+                sub_1 = """
+                    SELECT h.hub_aircraft_key FROM hub_aircraft as h WHERE h.hub_aircraft_bk = %s
                 """
                 
-                cursor.execute(sql_lnk, (l1, l2, l3, l4, l5))
+                cursor.execute(sub_1, (l3))
+                
+                result = cursor.fetchone()[0]
+                
+                sql_lnk = """
+                    INSERT INTO lnk_seat_on_aircraft (lnk_seat_on_aircraft, hub_seat_key, hub_aircraft_key, lnk_load_dts, lnk_rec_src)
+                    VALUES (%s, %s, %s, %s, %s)
+                """
+                
+                cursor.execute(sql_lnk, (l1, l2, result, l4, l5))
                 
                 pg_connector.commit()
             except Exception as e:
@@ -228,6 +238,128 @@ def dmp_seats():
         consumer.close()
         cursor.close()
         pg_connector.close()
+
+# Flights
+def dmp_flights():
+
+    consumer = KafkaConsumer(
+        "postgres.public.flights",
+        bootstrap_servers=["broker1:29092"],
+        group_id="dmp-group",
+        auto_offset_reset='earliest',
+        enable_auto_commit=True,
+        value_deserializer=lambda x: x.decode('utf-8')
+    )
+
+    pg_connector = psycopg2.connect(
+        host="postgres_dwh",
+        port=5432,
+        database="postgres",
+        user="postgres",
+        password="postgres"
+    )
+    
+    cursor = pg_connector.cursor()
+
+    print("consumer and postgres connector was created")
+
+    try:
+        for message in consumer:
+            message_value = message.value
+            print(f"message value: {message_value}")
+            
+            row = json.loads(message_value)["payload"]["after"]
+            print(f"insert values: {row}")
+
+            try:
+                h2 = row["flight_id"]
+                h3 = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                h4 = 'kafka'
+                h1 = str(hash(h2) + hash(h3) + hash(h4))
+            	
+                s1 = h1
+                s2 = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                s3 = ""
+                s4 = "kafka"
+                s5 = row["flight_no"]
+                s6 = row["scheduled_departure"]
+                s7 = row["scheduled_arrival"]
+                s8 = row["status"]
+                s9 = row["actual_departure"]
+                s10 = row["actual_arrival"]
+                
+                l2 = h1
+                l3 = row["departure_airport"]
+                l4 = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                l5 = "kafka"
+                l1 = str(hash(l2) + hash(l3) + hash(l4) + hash(l5))
+                
+                ll2 = h1
+                ll3 = row["departure_airport"]
+                ll4 = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                ll5 = "kafka"
+                ll6 = str(hash(ll2) + hash(ll3) + hash(ll4) + hash(ll5))
+            	
+                sql_hub = """
+    				INSERT INTO hub_flight (hub_flight_key, hub_flight_bk, hub_load_dts, hub_rec_src)
+    				VALUES (%s, %s, %s, %s)
+				"""
+				
+                cursor.execute(sql_hub, (h1, h2, h3, h4))
+            	
+                sql_sat = """
+            		INSERT INTO sat_flight (hub_flight_key, sat_load_dts, hash_diff, sat_rec_src, flight_no, scheduled_departure, scheduled_arrive, status, actual_departure, actual_arrival)
+            		VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            	"""
+                cursor.execute(sql_sat, (s1, s2, s3, s4, s5, s6, s7, s8, s9, s10))
+                
+                pg_connector.commit()
+                
+                sub_1 = """
+                    SELECT h.hub_airport_key FROM hub_airport as h WHERE h.hub_airport_bk = %s
+                """
+                
+                cursor.execute(sub_1, (l3))
+                
+                result = str(cursor.fetchone()[0])
+                
+                sql_lnk = """
+                    INSERT INTO lnk_flight_departure_airport (lnk_flight_departure_airport, hub_flight_key, hub_airport_key, lnk_load_dts, lnk_rec_src)
+                    VALUES (%s, %s, %s, %s, %s)
+                """
+                
+                cursor.execute(sql_lnk, (l1, l2, result, l4, l5))
+                
+                pg_connector.commit()
+                
+                sub_2 = """
+                    SELECT h.hub_airport_key FROM hub_airport as h WHERE h.hub_airport_bk = %s
+                """
+                
+                cursor.execute(sub_2, (ll3))
+                
+                result = str(cursor.fetchone()[0])
+                
+                sql_lnk = """
+                    INSERT INTO lnk_flight_arrive_airport (lnk_flight_arrive_airport, hub_flight_key, hub_airport_key, lnk_load_dts, lnk_rec_src)
+                    VALUES (%s, %s, %s, %s, %s)
+                """
+                
+                cursor.execute(sql_lnk, (ll1, ll2, result, ll4, ll5))
+                
+                pg_connector.commit()
+                
+            except Exception as e:
+                print(f"Database error: {e}")
+                pg_connector.rollback()
+    except Exception as e:
+            print("Closing consumer due to error\n")
+            print(e)
+    finally:
+        consumer.close()
+        cursor.close()
+        pg_connector.close()
+    
 
 def main():
     threads = []
@@ -243,5 +375,10 @@ def main():
     dmp_seats_thread = threading.Thread(target=dmp_seats)
     threads.append(dmp_seats_thread)
     dmp_seats_thread.start()
+    
+    dmp_flights_thread = threading.Thread(target=dmp_flights)
+    threads.append(dmp_flights_thread)
+    dmp_flights_thread.start()
+
 
 main()
